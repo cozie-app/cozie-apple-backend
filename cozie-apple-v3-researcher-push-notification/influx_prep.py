d56e52c4377d99d2c48a4e4e934c256fea623357
Mario Frei <mario.frei@gmx.net>
2024-03-27 02:29:24 +0800

    Add push notification lambda function

---
+++ cozie-apple-v3-researcher-push-notification/influx_prep.py
@@ -0,0 +1,150 @@
+"""
+Module to sanitize strings that are used for InfluxDB queries.
+Purpose:
+  - Remove special characters
+  - Escape special characters
+  - Enforce string types where necessary
+Source: https://docs.influxdata.com/influxdb/v1/write_protocols/line_protocol_tutorial/#special-characters-and-keywords
+"""
+
+remove_character_sql = ['\\', '\'', '"']
+escape_character_tag_key = [',', '=', ' ']
+escape_character_tag_value = [',', '=', ' ']
+escape_character_field_key = [',', '=', ' ']
+escape_character_field_value = ['"']
+escape_character_measurement = [',', '=', ' ']
+
+def test():
+  """
+  Test functions that prints 'test'
+
+  Arguments
+  ----------
+    -
+
+  Returns
+  -------
+    -
+  """
+  print('test')
+  return
+
+def remove_character(input_string, remove_characters):
+  """
+  Removes special characters from string
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed
+    - remove_characters, list, List of strings/characters, that are removed from input_string
+
+  Returns
+  -------
+    - output_string, str, result from removing remove_characters from input_string
+  """
+  output_string = input_string
+  for character in remove_characters:
+    output_string = output_string.replace(character, '')
+  return output_string
+
+def escape_character(input_string, escape_strings):
+  """
+  Escapes special characters from string
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed
+    - escape_characters, list, List of strings/characters, that are escaped in input_string
+
+  Returns
+  -------
+    - output_string, str, result from escaping escape_characters in input_string
+  """
+  output_string = input_string
+  for character in escape_strings:
+    output_string = output_string.replace(character, '\''+character)
+  return output_string
+
+def measurement(input_string):
+  """
+  Removes and escapes sepcial characters from string containing an InfluxDB measurement
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed/escaped
+
+  Returns
+  -------
+    - output_string, str, result from removing/escaping special characters from input_string
+  """
+  output_string = str(input_string)
+  output_string = remove_character(output_string, remove_character_sql)
+  output_string = escape_character(output_string, escape_character_measurement)
+  return output_string
+
+def tag_key(input_string):
+  """
+  Removes and escapes sepcial characters from string containing an InfluxDB tag key
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed/escaped
+
+  Returns
+  -------
+    - output_string, str, result from removing/escaping special characters from input_string
+  """
+  output_string = str(input_string)
+  output_string = remove_character(output_string, remove_character_sql)
+  output_string = escape_character(output_string, escape_character_tag_key)
+  return output_string
+
+def tag_value(input_string):
+  """
+  Removes and escapes sepcial characters from string containing an InfluxDB tag value
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed/escaped
+
+  Returns
+  -------
+    - output_string, str, result from removing/escaping special characters from input_string
+  """
+  output_string = remove_character(input_string, remove_character_sql)
+  output_string = escape_character(output_string, escape_character_tag_value)
+  return output_string
+
+def field_key(input_string):
+  """
+  Removes and escapes sepcial characters from string containing an InfluxDB field key
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed/escaped
+
+  Returns
+  -------
+    - output_string, str, result from removing/escaping special characters from input_string
+  """
+  output_string = str(input_string)
+  output_string = remove_character(output_string, remove_character_sql)
+  output_string = escape_character(output_string, escape_character_field_key)
+  return output_string
+
+def field_value(input_string):
+  """
+  Removes and escapes sepcial characters from string containing an InfluxDB field value
+
+  Arguments
+  ----------
+    - input_string, str, String from which charcters are removed/escaped
+
+  Returns
+  -------
+    - output_string, str, result from removing/escaping special characters from input_string
+  """
+  output_string = str(input_string)
+  output_string = remove_character(output_string, remove_character_sql)
+  output_string = escape_character(output_string, escape_character_field_value)
+  return output_string
---
+++ cozie-apple-v3-researcher-push-notification/lambda_function.py
@@ -0,0 +1,134 @@
+# Send notifications to Cozie-Apple app
+# Author: Mario Frei, 2024
+# Project: Cozie
+# Sources:
+#  - https://documentation.onesignal.com/reference/push-channel-properties
+
+import requests
+import json
+import os
+import influx_prep
+from influxdb import InfluxDBClient
+import pandas as pd
+
+def lambda_handler(event, context):
+    print(f'event ({type(event)})\n {event}')
+    
+    # Parse payload
+    if 'body' in event:
+        # Parse payload when function URL is called
+        payload = json.loads(event['body'])
+    else: 
+        # Parse payload when API Gateway is called
+        payload = event
+        
+    print('Payload:\n', payload)
+    
+    if 'id_experiment' in payload:
+        id_experiment = payload['id_experiment']
+    else:
+        return {
+            'statusCode': 400,
+            'body': 'Experiment ID is missing'
+        }
+        
+    if 'id_participant' in payload:
+        id_participant = payload['id_participant']
+    else:
+        return {
+            'statusCode': 400,
+            'body': 'Participant ID is missing'
+        }
+        
+    if 'id_password' in payload:
+        id_password = payload['id_password']
+    else:
+        return {
+            'statusCode': 400,
+            'body': 'id_password is missing'
+        }
+    
+    message = ''
+    heading = ''
+    subtitle = ''
+    buttons = ''
+    
+    if 'message' in payload:
+        message = payload['message']
+
+    if 'heading' in  payload:
+        heading = payload['heading']
+    
+    if 'subtitle' in  payload:
+        subtitle = payload['subtitle']
+        
+    if 'buttons' in payload:
+        buttons = payload['buttons']
+
+    # Sanitize payload
+    id_experiment = influx_prep.measurement(id_experiment)
+    id_participant = influx_prep.tag_value(id_participant)
+    id_password = influx_prep.tag_value(id_password)
+    
+    # Initialize InfluxDB client
+    influx_client = InfluxDBClient(os.environ['db_host'], 
+                                   os.environ['db_port'],
+                                   os.environ['db_user'],
+                                   os.environ['db_password'],
+                                   os.environ['db_name'],
+                                   ssl=True, 
+                                   verify_ssl=True)
+    
+    # Query DB
+    db_name = os.environ['db_name']
+    influx_query = f'SELECT * FROM "{db_name}"."autogen"."{id_experiment}" WHERE "id_participant"=\'{id_participant}\' AND "id_password"=\'{id_password}\' ORDER BY DESC LIMIT 1'
+    print('query influx: ', influx_query)
+    
+    result = influx_client.query(influx_query)
+    print('result:\n', result)
+    
+    # Get OneSignal ID
+    response_body = dict()
+    if len(result)<1:
+        # Handle empty result set
+        id_onesignal = ''
+        id_password_db = ''
+    else:
+        # Convert result from database to dataframe
+        df = pd.DataFrame.from_dict(result[id_experiment])
+        id_onesignal = df['id_onesignal'][0]
+        print(f'id_onesignal: {id_onesignal}')
+    
+    if id_onesignal == '':
+        return {
+            'statusCode': 400,
+            'body': 'No valid OneSignal Player ID found.'
+        }
+    
+    # Send push notification
+    token = os.environ["onesignal_token"] # provided by OneSignal. Can be found on the Keys & IDs page on the OneSignal Dashboard for this particular app
+    app_id = os.environ["onesignal_app_id"] # provided by OneSignal, needs to be implemented in the app
+    
+    header = {"Content-Type": "application/json; charset=utf-8",
+              "Authorization": "Basic "+ token}
+    
+    payload_out = {"app_id": app_id,
+                   "include_player_ids": [id_onesignal],
+                   "contents": {"en": message},
+                   "headings": {"en": heading},
+                   "subtitle": {"en": subtitle},
+                   }
+    if buttons != '':
+        payload_out['buttons'] = buttons
+        
+    print('payload_out\n', payload_out)
+    
+    response = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload_out))
+    print('response.content: ', response.content)
+    print('req.reason: ', response.reason)
+    
+    return {
+        'headers': {'Content-Type': 'application/json; charset=utf-8'},
+        'statusCode': response.status_code,
+        'body': response.content
+    }
